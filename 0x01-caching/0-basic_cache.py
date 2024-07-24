83f3402f5f5bcfa22717b5d7de0419982bc196dd
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-07-24 13:04:27 +0000

    cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,8 +12,8 @@ class BasicCache(BaseCaching):
         If key or item is None, this method should not do anything."""
 
         if key is not None and item  is not None:
-            return self.cache_data[key] = item
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ returned the assign itemm """
-        return self.cache_data.get(key, none)
+        """ return the assign itemm """
+        return self.cache_data.get(key, None)
