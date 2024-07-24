75732a349aeb3afd6401c7bcd3e3f47693dcede7
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-07-24 16:59:21 +0000

    fifo

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" a cache system module"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    """an object that allows storing and receiving of items
+    in the dictionary and also implement the fifo algorithm"""
+    def __init__(self):
+        """initializes the cach"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ a put method to assign items to the dictionary"""
+        if key is not None and item is not None:
+            return 
+        self.cache_data[key] = item
+        if (len.self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
+
+    def get(self, key):
+        """get the keys in the dictionary"""
+        return self.cache_data.get(key, None) 
---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a cache module sytem """
+
+
+class LIFOCache(BaseCaching):
+    """a lif
